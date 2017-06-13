02cb8b5cb918797e34f6673030354786bbbfd80b
Yu-An Ko <handcuffanndo@gmail.com>
2017-06-13 15:25:25 +0800

    use seed.rb to generate user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理員
+u = User.new
+u.email = "admin@rails.com"
+u.password = "111111"
+u.password_confirmation = "111111"
+u.is_admin = true
+u.save
+
+# 一般
+u = User.new
+u.email = "user@rails.com"
+u.password = "111111"
+u.password_confirmation = "111111"
+u.is_admin = false
+u.save
