3cbee648c1063b82f16e809ae31fc7cca41ff4e5
Yu-An Ko <handcuffanndo@gmail.com>
2017-06-13 15:38:47 +0800

    fix image uploader details

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,6 +38,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :stock)
+    params.require(:product).permit(:name, :description, :price, :stock, :image)
   end
 end
